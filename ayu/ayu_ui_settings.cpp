eed6fe4254c3670e48b680a7e0b61f642d3a40e8
ZavaruKitsu <alexeyzavar@gmail.com>
2023-08-27 13:21:46 +0300

    feat: AyuGram changes
    Co-authored-by: MaxPlays35 <35427519+maxplays35@users.noreply.github.com>

---
+++ ayu/ayu_ui_settings.cpp
@@ -0,0 +1,40 @@
+// This is the source code of AyuGram for Desktop.
+//
+// We do not and cannot prevent the use of our code,
+// but be respectful and credit the original author.
+//
+// Copyright @Radolyn, 2025
+#include "ayu_ui_settings.h"
+
+#include <utility>
+
+namespace AyuUiSettings {
+
+QString monoFont;
+double wideMultiplier = 1.0;
+
+void setMonoFont(QString newFont) {
+	monoFont = std::move(newFont);
+}
+
+QString getMonoFont() {
+	return monoFont;
+}
+
+void setWideMultiplier(double val) {
+	wideMultiplier = val;
+}
+
+bool isWideMultiplied() {
+	return abs(wideMultiplier - 1.0) > 0.01;
+}
+
+int getWideMultiplied(int width, double mult) {
+	if (!isWideMultiplied()) {
+		return width;
+	}
+	const auto res = width * (wideMultiplier * mult);
+	return std::max(width, static_cast<int>(std::round(res)));
+}
+
+}
